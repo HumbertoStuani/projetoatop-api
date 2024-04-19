b0880024cd53940db486c9fe142e3c9a9ae819ad
Meiado <gabrielmeiado@hotmail.com>
2024-04-19 12:05:34 -0300

    Adição de rota de recuperação de imagem através do id da Denuncia

--- src/main/java/br/unoeste/ativooperante/controllers/ImagemRestController.java
+++ src/main/java/br/unoeste/ativooperante/controllers/ImagemRestController.java
@@ -2,9 +2,14 @@ package br.unoeste.ativooperante.controllers;
 
 import br.unoeste.ativooperante.db.mongo.model.Imagem;
 import br.unoeste.ativooperante.db.repository.ImagemRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -14,13 +19,18 @@ public class ImagemRestController {
 
     @Autowired
     private ImagemRepository imagemRepository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
-//    @PostMapping
-//    public Imagem salvarImagem(@RequestBody Imagem imagem) {
-//        return imagemRepository.save(imagem);
-//    }
-
-    @GetMapping(value = "/exibir/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
+    @GetMapping(value = "/exibir/{id}")
+    public ResponseEntity<byte[]> recuperarImagemPorDenuncia(@PathVariable long id) {
+        Query query = new Query(Criteria.where("denuncia.$id").is(id));
+        Imagem imagem = mongoTemplate.findOne(query, Imagem.class);
+        if (imagem != null && imagem.getDados() != null)
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(imagem.getDados());
+        return ResponseEntity.notFound().build();
+    }
+    @GetMapping(value = "/imagens/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
     public @ResponseBody byte[] recuperarImagem(@PathVariable String id) {
         Imagem imagem = imagemRepository.findById(id).orElse(null);
         if (imagem == null)
