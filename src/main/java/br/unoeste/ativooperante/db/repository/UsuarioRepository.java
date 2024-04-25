86e50e1c4078c48f771f66f39251093cb5e2bcd9
Meiado <gabrielmeiado@hotmail.com>
2024-04-25 15:58:53 -0300

    Adição do método de busca de usuário por email

--- src/main/java/br/unoeste/ativooperante/db/repository/UsuarioRepository.java
+++ src/main/java/br/unoeste/ativooperante/db/repository/UsuarioRepository.java
@@ -3,5 +3,8 @@ package br.unoeste.ativooperante.db.repository;
 import br.unoeste.ativooperante.db.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+    Optional<Usuario> findByEmail(String email);
 }
